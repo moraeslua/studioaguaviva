6b2e3dd87c59fd067c3ef35d5102eb2a3c714510
moraes <moraeslua66@gmail.com>
2021-12-21 15:35:12 -0300

    Dev: Adiciona funcionalidade de fechar o modal ao clicar no X

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ const portfolioImgs = document.querySelectorAll('.container-image > div > img');
 const modalContainer = document.querySelector('.modal-container');
 
 function hideAndResetModal(event) {
-  if(event.target.classList.contains('modal-container')) {
-    event.target.classList.remove('show');
+  if(event.target.classList.contains('modal-container') || event.target.classList.contains('close-modal-button')) {
+    modalContainer.classList.remove('show');
     const modalContent = document.querySelector('.modal-content');
     modalContent.innerHTML = '';
   }
 }
 
-modalContainer.addEventListener('click', hideAndResetModal)
+modalContainer.addEventListener('click', hideAndResetModal);
 
 function showBigImage(event){
   const clickedPhoto = event.target;
@@ -30,10 +30,3 @@ function showBigImage(event){
 for (let child of portfolioImgs) {
   child.addEventListener('click', showBigImage);
 }
-
-// fazer com que a foto do event.target apareça dentro da div modal
-
-//identificar o event.target
-//adicionar ele as caracteristicas do modal
-//adicionar .show
-//remover tudo quando clicar em X
