0cfecd0cb55e018f9c9ef15408ff022ea68be1ef
moraes <moraeslua66@gmail.com>
2021-12-22 00:20:37 -0300

    Dev: Adiciona novas funcionalidades e alguns acabamentos na pag principal

--- script.js
+++ script.js
@@ -30,3 +30,21 @@ function showBigImage(event){
 for (let child of portfolioImgs) {
   child.addEventListener('click', showBigImage);
 }
+
+//HOVER DE .GO-TO-GALLERY
+const containerGoToGallery = document.querySelector('.go-to-gallery');
+
+containerGoToGallery.addEventListener('mouseover', () => {
+  const circle = document.querySelector('#home-circle');
+  const arrow = document.querySelector('#arrow');
+  circle.style.backgroundColor = ' rgb(4, 36, 41)';
+  circle.style.transition= '0.3s linear';
+  arrow.style.color = 'white';
+});
+
+containerGoToGallery.addEventListener('mouseout', () => {
+  const circle = document.querySelector('#home-circle');
+  const arrow = document.querySelector('#arrow');
+  circle.style.backgroundColor = 'unset';
+  arrow.style.color = 'unset';
+});
